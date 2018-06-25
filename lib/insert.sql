a10b8bd7d45d97c599ad0af5e7a0d17162e5f466
Maverick Tesini <mavericktesini@gmail.com>
2018-06-25 22:47:48 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,34 @@
+INSERT INTO bears (
+  name
+) VALUES (
+  "Mr. Chocolate",
+  "Rowdy",
+  "Tabitha",
+  "Sergeant Brown",
+  "Melissa",
+  "Grinch",
+  "Wendy",
+  NULL
+);
+
+
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
+--  (the bear that killed Tim didn't have a name;
+--    refer back to how to create a record that doesn't have one value)
+
+
+-- id	integer
+-- name	text
+-- age	integer
+-- gender	char(1)(The choices would be "M" or "F")
+-- color	text
+-- temperament	text
+-- alive	boolean
