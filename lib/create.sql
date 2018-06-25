d4f95e9ebb48adc7d9d83cef39f2d450cbb4f300
Maverick Tesini <mavericktesini@gmail.com>
2018-06-25 22:40:10 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE bears (
+  id TEXT PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+
+-- id	integer
+-- name	text
+-- age	integer
+-- gender	char(1)(The choices would be "M" or "F")
+-- color	text
+-- temperament	text
+-- alive	boolean
